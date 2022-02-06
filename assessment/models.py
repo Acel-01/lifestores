298fbe4c575fb5e36cba3131714c5b6d52ee4c11
Acel-01 <emekaaladimma@gmail.com>
2022-02-06 05:56:30 +0100

    modified product model

--- assessment/models.py
+++ assessment/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(null=True, blank=True)
-    sku = models.CharField(max_length=6, null=True, blank=True)
-    price = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    name = models.CharField(max_length=100,null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    sku = models.CharField(max_length=6, null=False, blank=False)
+    price = models.DecimalField(max_digits=8, decimal_places=2, null=False, blank=False)
+    image = models.ImageField(upload_to='lifestores-assessment/',null=False, blank=False)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
